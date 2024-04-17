d3c2b3af8cb299e0dedd79b62d9d2e9226dda60f
Kyogesh1510 <130115608+Kyogesh1510@users.noreply.github.com>
2024-04-17 18:19:06 +0530

    Time: 2 ms (97.57%), Space: 41.7 MB (52.57%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,26 @@
+
+
+class Solution {
+
+    public boolean uniqueOccurrences(int[] arr) {
+
+        if (arr.length == 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        Set<Integer> set = new HashSet<>(map.values());
+
+        return map.size() == set.size();
+    }
+}
+
